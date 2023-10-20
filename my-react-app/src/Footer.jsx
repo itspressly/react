713bd6df6c3081372c7b2335d1868ed077ccbcad
it'smepressly <130832755+itspressly@users.noreply.github.com>
2023-10-19 21:25:06 -0700

    first commit

---
+++ my-react-app/src/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} THE banguse webpage</p>
+        </footer>
+    );
+}
+
+export default Footer
---
+++ my-react-app/src/Header.jsx
@@ -0,0 +1,18 @@
+function Header(){
+
+    return(
+        <header>
+        <h1>My website </h1>
+        <nav>
+            <ul>
+                <a href="#"><li>home</li></a>
+                <a href="#"><li>about</li></a>
+                <a href="#"><li>services</li></a>
+            </ul>
+        </nav>
+        <hr />
+        </header>
+    )
+}
+
+export default Header
